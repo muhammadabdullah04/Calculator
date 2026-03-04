0f798a2709d4dfe22680b7a7d37d2b67520cc8cc
Muhammad Abdullah <113317802+muhammadabdullah04@users.noreply.github.com>
2026-03-04 18:53:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+st.set_page_config(page_title="Calculator", layout="centered")
+
+st.title("🧮" \
+"" \
+"" \
+" Calculator")
+
+# Store expression
+if "exp" not in st.session_state:
+    st.session_state.exp = ""
+
+# Functions
+def press(value):
+    st.session_state.exp += value
+
+def clear():
+    st.session_state.exp = ""
+
+def calculate():
+    try:
+        expression = st.session_state.exp
+        expression = expression.replace("➕","+")
+        expression = expression.replace("➖","-")
+        expression = expression.replace("✖","*")
+        expression = expression.replace("➗","/")
+        st.session_state.exp = str(eval(expression))
+    except:
+        st.session_state.exp = "Error"
+
+# Keyboard input
+user_input = st.text_input("Type calculation and press Enter", value=st.session_state.exp)
+
+if user_input != st.session_state.exp:
+    st.session_state.exp = user_input
+
+
+
+st.divider()
+
+# Buttons
+rows = [
+    ["7","8","9","➗"],
+    ["4","5","6","✖"],
+    ["1","2","3","➖"],
+    ["C","0","=","➕"]
+]
+
+operators = ["➕","➖","✖","➗"]
+
+for row in rows:
+    cols = st.columns(4)
+
+    for i in range(4):
+
+        if row[i] == "=":
+            cols[i].button("=", use_container_width=True, on_click=calculate, type="primary")
+
+        elif row[i] == "C":
+            cols[i].button("C", use_container_width=True, on_click=clear)
+
+        elif row[i] in operators:
+            cols[i].button(row[i], use_container_width=True, on_click=press, args=(row[i],), type="primary")
+
+        else:
+            cols[i].button(row[i], use_container_width=True, on_click=press, args=(row[i],))
